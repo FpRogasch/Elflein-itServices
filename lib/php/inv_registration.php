a62df41428d6f267726b4354993ed37368249976
FpRogasch <fpoblete.develop@gmail.de>
2019-12-04 14:20:54 +0100

    Alert at registration.

--- lib/php/inv_registration.php
+++ lib/php/inv_registration.php
@@ -4,22 +4,24 @@
   $connect = OpenConnection();
 
   $categorie = $_POST['Categorie'];
-  $sub_categorie = $_POST['Sub_categorie'];
   $model = $_POST['Model'];
   $location = $_POST['Location'];
   $owner = $_POST['Owner'];
   $imei = $_POST['Imei'];
-  $serieNumber = ['SerieNumber'];
-  $description = ['Description'];
+  $serieNumber = $_POST['SerieNumber'];
+  $description = $_POST['Description'];
 
-  $sql = "INSERT INTO inventory (Categorie, Sub_categorie, Model, Location, IMEI, Serie_number, Owner, Description)
-                        VALUES ('$categorie', '$sub_categorie', '$model', '$location', '$imei', '$serieNumber', '$owner', '$description')";
+  $sql = "INSERT INTO inv_it_test (Categorie, Model, Location, IMEI, Serie_number, Owner, Description)
+                        VALUES ('$categorie', '$model', '$location', '$imei', '$serieNumber', '$owner', '$description')";
 
   if(!mysqli_query($connect,$sql)){
     echo "Not Inserted";
   }
   else {
-    echo "Inserted";
+    echo '<script>
+      alert("record successfully inserted in the database");
+      window.history.go(-1);
+    </script>';
   }
 
   CloseConnection($connect);
